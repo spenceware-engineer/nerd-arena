b63dbea808e8f029f114839f22f53c8b6a59f26f
Cindy Spence <spence@Cynthia.attlocal.net>
2023-01-10 22:57:10 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <div className="navbar-top">
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+        <div className="square red"></div>
+      </div>
+      <div className="navbar">
+        <div className="navbar-side">
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+          <div className="square yellow"></div>
+        </div>
+        <img src='rubik.png' alt="rubik's cube icon" className="icon" />
+        <div className="navbar-side">
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+          <div className="square blue"></div>
+        </div>
+      </div>
+      <div className="navbar-bottom">
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+        <div className="square orange"></div>
+      </div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/NevGDFBfQGw"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen>
+      </iframe>
+    </>
   );
 }
 
